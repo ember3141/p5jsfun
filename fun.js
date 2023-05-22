4e444c7a7da23651c4ef5169585c7b71bed0be0e
ember314 <111328444+ember3141@users.noreply.github.com>
2023-05-22 18:30:09 +0000

    modified:   fun.js

--- fun.js
+++ fun.js
@@ -9,78 +9,105 @@ const keys = {
 		s: [
 			[10, 14, 70, 90],
 			[10, 16.25, 80, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	cs: {
 		s: [
 			[14, 18, 70, 80]
-		]
+		],
+		color: [255,0,0],
+		touching: false
 	},
 	d: {
 		s: [
 			[16.25, 23, 80, 90],
 			[18, 21.75, 70, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	ds: {
 		s: [
 			[21, 25, 70, 80]
-		]
+		],
+		color: [255,0,0],
+		touching: false
 	},
 	e: {
 		s: [
 			[23, 29.25, 80, 90],
 			[25, 29.25, 70, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	f: {
 		s: [
 			[29.25, 33, 70, 90],
 			[29.25, 35.5, 80, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	fs: {
 		s: [
 			[33, 37, 70, 80]
-		]
+		],
+		color: [255,0,0],
+		touching: false
 	},
 	g: {
 		s: [
 			[35.5, 42.25, 80, 90],
 			[37, 40.75, 70, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	gs: {
 		s: [
 			[40.75, 44.75, 70, 80]
-		]
+		],
+		color: [255,0,0],
+		touching: false
 	},
 	a: {
 		s: [
 			[42, 49, 80, 90],
 			[44, 47.75, 70, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	},
 	as: {
 		s: [
 			[47.75, 51.75, 70, 80]
-		]
+		],
+		color: [255,0,0],
+		touching: false
 	},
 	b: {
 		s: [
 			[49, 55.25, 80, 90],
 			[51.75, 55.25, 70, 90]
-		]
+		],
+		color: [255,255,255],
+		touching: false
 	}
 };
 
 var cont = {
 	slide : {
-		x:50,
-		y:50,
+		sx:5,
+		sy:40,
+		ex:5,
+		ey:50,
 		min:0,
 		max:100,
 		inc:1,
+		val:0.5
 	}
 };
 
@@ -112,23 +139,39 @@ function draw() {
 	clear();
 	m.x = f(mouseX * IX / CX);
 	m.y = f(mouseY * IY / CY);
-
+	
 	img.loadPixels();
+	drawcontrol("slide");
+
 	for (var i = 0; i < img.pixels.length / 4; i++) {
 		j = i * 4;
 		y = f(i / IX);
 		x = f(i - (y * IX));
 
+for(var l=0;l<Object.keys(keys).length;l++){
+		if (s(keys[Object.keys(keys)[l]].s)) {
+			// if(x==m.x&&y==m.y){
+			// 	keys[Object.keys(keys)[l]].touching=true;
+			// } else {
+			// 	keys[Object.keys(keys)[l]].touching=false;
+			// }
 
-		if (s(keys.c.s) || s(keys.d.s) || s(keys.e.s) || s(keys.f.s) || s(keys.g.s) || s(keys.a.s) || s(keys.b.s)) {
-			img.pixels[j] = 255;
-			img.pixels[j + 1] = 255;
-			img.pixels[j + 2] = 255;
+			if(keys[Object.keys(keys)[l]].touching==true){
+			img.pixels[j] = keys[Object.keys(keys)[l]].color[0];
+			img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1];
+			img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2];
+			}else {
+				img.pixels[j] = keys[Object.keys(keys)[l]].color[0];
+				img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1];
+				img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2];
+			}
+			
 		}
+		
+	
+		}
+	
 
-		if (s(keys.cs.s) || s(keys.ds.s) || s(keys.fs.s) || s(keys.gs.s) || s(keys.as.s)) {
-			img.pixels[j] = 200;
-		}
 		if (y == f(m.ly) && x == f(m.lx) && (m.lx != m.llx || m.ly != m.lly)) {
 			// img.pixels[j] = 0;
 			// img.pixels[j + 1] = 0;
@@ -149,6 +192,8 @@ function draw() {
 
 		
 	}
+	
+
 	img.updatePixels();
 	image(img, 0, 0, CX, CY, 0, 0);
 	m.llx = m.lx;
@@ -164,6 +209,14 @@ function f(xx) {
 function s(xmin) {
 	for (var i = 0; i < xmin.length; i++) {
 		if (x > f((IX * xmin[i][0]) * 0.01) && x < f((IX * xmin[i][1]) * 0.01) && y > f((IY * xmin[i][2]) * 0.01) && y < f((IY * xmin[i][3]) * 0.01)) {
+			return true;
+		}
+	}
+}
+
+function checks(xmin,xx,yy) {
+	for (var i = 0; i < xmin.length; i++) {
+		if (xx > f((IX * xmin[i][0]) * 0.01) && xx < f((IX * xmin[i][1]) * 0.01) && yy > f((IY * xmin[i][2]) * 0.01) && yy < f((IY * xmin[i][3]) * 0.01)) {
 			return true;
 		}
 	}
@@ -206,3 +259,20 @@ function mousePressed() {
 function mouseReleased() {
     m.down = false;
 }
+
+function drawcontrol(name){
+	var ct=cont[name];
+	var dist = Math.sqrt(Math.pow(ct.sx-ct.ex,2)+Math.pow(ct.sy-ct.ey,2));
+	// if(x==cont[name].sx&&y==cont[name].sy){
+	// img.pixels[j]=255;
+	// }
+	bline(ct.sx,ct.sy,ct.ex,ct.ey,55,55,55);
+	bline(ct.sx+1,ct.sy,ct.ex+1,ct.ey,55,55,55);
+	img.set(ct.sx, ct.sy+dist*ct.val, color(155,155,155));
+	img.set(ct.sx+1, ct.sy+dist*ct.val, color(155,155,155));
+	img.set(ct.sx+2, ct.sy+dist*ct.val, color(155,155,155));
+	img.set(ct.sx-1, ct.sy+dist*ct.val, color(155,155,155));
+	
+}
+
+//√((x2 – x1)² + (y2 – y1)²)
