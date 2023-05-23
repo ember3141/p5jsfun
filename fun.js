c9e408f42c2768db0f65ae20b7f4f88f00078d68
ember314 <111328444+ember3141@users.noreply.github.com>
2023-05-23 13:19:11 +0000

    controls

--- fun.js
+++ fun.js
@@ -10,7 +10,7 @@ const keys = {
 			[10, 14, 70, 90],
 			[10, 16.25, 80, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -18,7 +18,7 @@ const keys = {
 		s: [
 			[14, 18, 70, 80]
 		],
-		color: [255,0,0],
+		color: [255, 0, 0],
 		touching: false,
 		pressed: false
 	},
@@ -27,7 +27,7 @@ const keys = {
 			[16.25, 23, 80, 90],
 			[18, 21.75, 70, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -35,7 +35,7 @@ const keys = {
 		s: [
 			[21, 25, 70, 80]
 		],
-		color: [255,0,0],
+		color: [255, 0, 0],
 		touching: false,
 		pressed: false
 	},
@@ -44,7 +44,7 @@ const keys = {
 			[23, 29.25, 80, 90],
 			[25, 29.25, 70, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -53,7 +53,7 @@ const keys = {
 			[29.25, 33, 70, 90],
 			[29.25, 35.5, 80, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -61,7 +61,7 @@ const keys = {
 		s: [
 			[33, 37, 70, 80]
 		],
-		color: [255,0,0],
+		color: [255, 0, 0],
 		touching: false,
 		pressed: false
 	},
@@ -70,7 +70,7 @@ const keys = {
 			[35.5, 42.25, 80, 90],
 			[37, 40.75, 70, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -78,7 +78,7 @@ const keys = {
 		s: [
 			[40.75, 44.75, 70, 80]
 		],
-		color: [255,0,0],
+		color: [255, 0, 0],
 		touching: false,
 		pressed: false
 	},
@@ -87,7 +87,7 @@ const keys = {
 			[42, 49, 80, 90],
 			[44, 47.75, 70, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	},
@@ -95,7 +95,7 @@ const keys = {
 		s: [
 			[47.75, 51.75, 70, 80]
 		],
-		color: [255,0,0],
+		color: [255, 0, 0],
 		touching: false,
 		pressed: false
 	},
@@ -104,22 +104,22 @@ const keys = {
 			[49, 55.25, 80, 90],
 			[51.75, 55.25, 70, 90]
 		],
-		color: [255,255,255],
+		color: [255, 255, 255],
 		touching: false,
 		pressed: false
 	}
 };
 
-var cont = {
-	slide : {
-		sx:5,
-		sy:40,
-		ex:5,
-		ey:50,
-		min:0,
-		max:100,
-		inc:1,
-		val:0.5
+var controls = {
+	slide: {
+		sx: 5,
+		sy: 40,
+		ex: 5,
+		ey: 50,
+		min: 0,
+		max: 100,
+		inc: 1,
+		val: 0.5
 	}
 };
 
@@ -138,62 +138,60 @@ function setup() {
 	noSmooth();
 	noCursor();
 	m = {};
-	
+
 	m.llx = 0;
 	m.lly = 0;
 	m.lx = 0;
 	m.ly = 0;
-	m.down=false;
-	touchtrack = [false,0];
+	m.down = false;
+	touchtrack = [false, 0];
 }
 
 function draw() {
-// console.log(keys[Object.keys(keys)[1]].touching);
 	clear();
 	m.x = f(mouseX * IX / CX);
 	m.y = f(mouseY * IY / CY);
-	
+
 	img.loadPixels();
-	drawcontrol("slide");
- touchtrack[0] = false; 
+	
+	touchtrack[0] = false;
 	for (var i = 0; i < img.pixels.length / 4; i++) {
 		j = i * 4;
 		y = f(i / IX);
 		x = f(i - (y * IX));
 
-for(var l=0;l<Object.keys(keys).length;l++){
-		if (s(keys[Object.keys(keys)[l]].s)) {
+		for (var l = 0; l < Object.keys(keys).length; l++) {
+			if (s(keys[Object.keys(keys)[l]].s)) {
 
-			if(x==m.x&&y==m.y){
-				keys[Object.keys(keys)[l]].touching=true;
-				touchtrack=[true,l];
-			}	  
-			if(keys[Object.keys(keys)[l]].touching==true&&touchtrack[1]!=l){
-				keys[Object.keys(keys)[l]].touching=false; 
-			}
+				if (x == m.x && y == m.y) {
+					keys[Object.keys(keys)[l]].touching = true;
+					touchtrack = [true, l];
+				}
+				if (keys[Object.keys(keys)[l]].touching == true && touchtrack[1] != l) {
+					keys[Object.keys(keys)[l]].touching = false;
+				}
 
-			if(keys[Object.keys(keys)[l]].touching==true){
-				if(m.down==true){
-					img.pixels[j] = keys[Object.keys(keys)[l]].color[0]-100;
-					img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1]-100;
-					img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2]-100;
-					keys[Object.keys(keys)[l]].pressed=true;
+				if (keys[Object.keys(keys)[l]].touching == true) {
+					if (m.down == true) {
+						img.pixels[j] = keys[Object.keys(keys)[l]].color[0] - 100;
+						img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1] - 100;
+						img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2] - 100;
+						keys[Object.keys(keys)[l]].pressed = true;
+					} else {
+						img.pixels[j] = keys[Object.keys(keys)[l]].color[0] - 50;
+						img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1] - 50;
+						img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2] - 50;
+					}
 				} else {
-			img.pixels[j] = keys[Object.keys(keys)[l]].color[0]-50;
-			img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1]-50;
-			img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2]-50;
+					img.pixels[j] = keys[Object.keys(keys)[l]].color[0];
+					img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1];
+					img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2];
+				}
+
 			}
-			}else {
-				img.pixels[j] = keys[Object.keys(keys)[l]].color[0];
-				img.pixels[j + 1] = keys[Object.keys(keys)[l]].color[1];
-				img.pixels[j + 2] = keys[Object.keys(keys)[l]].color[2];
-			}
-			
+
 		}
-		
-	
-		}
-	
+
 
 		if (y == f(m.ly) && x == f(m.lx) && (m.lx != m.llx || m.ly != m.lly)) {
 			// img.pixels[j] = 0;
@@ -205,23 +203,30 @@ for(var l=0;l<Object.keys(keys).length;l++){
 			// img.pixels[j] = 255;
 			// img.pixels[j + 1] = 255;
 			// img.pixels[j + 2] = 255;
-			if(m.down==true){
+			if (m.down == true) {
 
-				bline(m.x, m.y, m.lx, m.ly, 255,0,0);
+				bline(m.x, m.y, m.lx, m.ly, 255, 0, 0);
 			} else {
-			bline(m.x, m.y, m.lx, m.ly, 0, 255, 255);
-		}
+				bline(m.x, m.y, m.lx, m.ly, 0, 255, 255);
+			}
 		}
 
-		
-	}
-	console.log(touchtrack);
-	if(touchtrack[0]==false){
-		keys[Object.keys(keys)[touchtrack[1]]].touching =false; 
-		 touchtrack = [false,0]; 
-	}
-// console.log(Object.keys(keys)[touchtrack[1]]); 
+		for(var n =0; n<Object.keys(controls).length; n++){
+			var ct = controls[Object.keys(controls)[n]];
+			var dist = Math.sqrt(Math.pow(ct.sx - ct.ex, 2) + Math.pow(ct.sy - ct.ey, 2));
+			if(m.x==ct.sx&&m.y>ct.sy&&m.y<ct.ey&&m.down==true){
+				//   ct.val=(m.y-ct.ey/dist)*0.01;
+			}
+		}
 
+	}
+
+	if (touchtrack[0] == false) {
+		keys[Object.keys(keys)[touchtrack[1]]].touching = false;
+		touchtrack = [false, 0];
+	}
+console.log(controls.slide.val);
+drawcontrol("slide");
 	img.updatePixels();
 	image(img, 0, 0, CX, CY, 0, 0);
 	m.llx = m.lx;
@@ -242,7 +247,7 @@ function s(xmin) {
 	}
 }
 
-function checks(xmin,xx,yy) {
+function checks(xmin, xx, yy) {
 	for (var i = 0; i < xmin.length; i++) {
 		if (xx > f((IX * xmin[i][0]) * 0.01) && xx < f((IX * xmin[i][1]) * 0.01) && yy > f((IY * xmin[i][2]) * 0.01) && yy < f((IY * xmin[i][3]) * 0.01)) {
 			return true;
@@ -281,26 +286,30 @@ function bline(x0, y0, x1, y1, c1, c2, c3) {
 
 
 function mousePressed() {
-   m.down = true;
+	m.down = true;
 }
 
 function mouseReleased() {
-    m.down = false;
+	m.down = false;
 }
 
-function drawcontrol(name){
-	var ct=cont[name];
-	var dist = Math.sqrt(Math.pow(ct.sx-ct.ex,2)+Math.pow(ct.sy-ct.ey,2));
-	// if(x==cont[name].sx&&y==cont[name].sy){
+function drawcontrol(name) {
+	var ct = controls[name];
+	var dist = Math.sqrt(Math.pow(ct.sx - ct.ex, 2) + Math.pow(ct.sy - ct.ey, 2));
+	// if(x==controls[name].sx&&y==controls[name].sy){
 	// img.pixels[j]=255;
 	// }
-	bline(ct.sx,ct.sy,ct.ex,ct.ey,55,55,55);
-	bline(ct.sx+1,ct.sy,ct.ex+1,ct.ey,55,55,55);
-	img.set(ct.sx, ct.sy+dist*ct.val, color(155,155,155));
-	img.set(ct.sx+1, ct.sy+dist*ct.val, color(155,155,155));
-	img.set(ct.sx+2, ct.sy+dist*ct.val, color(155,155,155));
-	img.set(ct.sx-1, ct.sy+dist*ct.val, color(155,155,155));
-	
+	bline(ct.sx, ct.sy, ct.ex, ct.ey, 55, 55, 55);
+	img.set(ct.sx, ct.sy + dist * ct.val, color(155, 155, 155));
+	img.set(ct.sx + 1, ct.sy + dist * ct.val, color(155, 155, 155));
+	// img.set(ct.sx + 2, ct.sy + dist * ct.val, color(155, 155, 155));
+	img.set(ct.sx - 1, ct.sy + dist * ct.val, color(155, 155, 155));
+
+	bline(ct.sx+1, ct.sy, ct.ex+1, ct.sy + dist * ct.val-1,255, 0, 0);
+	bline(ct.sx-1, ct.sy, ct.ex-1, ct.sy + dist * ct.val-1,255, 0, 0);
+	bline(ct.sx-1, ct.ey, ct.ex-1, ct.sy + dist * ct.val+1,255, 0, 0);
+	bline(ct.sx+1, ct.ey, ct.ex+1, ct.sy + dist * ct.val+1,255, 0, 0);
+
 }
 
 //√((x2 – x1)² + (y2 – y1)²)
